b9e4fce16a89c0023d45778f527ae71de3bb08b2
ZakGmd <Ghoumidatezakaria@gmail.com>
2024-05-17 01:27:19 +0100

    adding a mock data to work with !

--- app/api/graphQL/route.tsx
+++ app/api/graphQL/route.tsx
@@ -3,6 +3,76 @@ import { ApolloServer } from '@apollo/server';
 import { gql } from 'graphql-tag';
 import { NextRequest } from 'next/server';
 
+const movies = [
+    {
+      id: 1 ,
+      title: 'The little things' ,
+      imageUrl: '/dzlWlpr.jpg' ,
+      description: '' ,  
+    },
+    {
+      id: 2 ,
+      title: 'The Equalizer' ,
+      imageUrl: '' ,
+      description: '' ,    
+    },
+    {
+      id: 3 ,
+      title: 'Her' ,
+      imageUrl: '' ,
+      description: '' ,    
+    },
+    {
+      id: 4 ,
+      title: 'Ip Man' ,
+      imageUrl: '' ,
+      description: '' ,  
+    },
+    {
+      id: 5 ,
+      title: 'The book of Eli ' ,
+      imageUrl: '' ,
+      description: '' ,  
+    }
+] ;
+const series = [
+    {
+      id: 1 ,
+      title: 'Peaky Blinders' ,
+      imageUrl: '' ,
+      description: '' ,  
+      saisons: 6
+    },
+    {
+      id: 2 ,
+      title: 'Blacklist' ,
+      imageUrl: '' ,
+      description: '' ,
+      saisons: 10    
+    },
+    {
+      id: 3 ,
+      title: 'Dark' ,
+      imageUrl: '' ,
+      description: '' ,  
+      saisons: 3  
+    },
+    {
+      id: 4 ,
+      title: 'Vikings' ,
+      imageUrl: '' ,
+      description: '' ,  
+      saisons: 6
+    },
+    {
+      id: 5 ,
+      title: 'Mr. Robot' ,
+      imageUrl: '' ,
+      description: '' ,  
+      saisons: 4
+    }
+] ;
+
 const typeDefs = gql`
   type Query {
       fetchMovies: [Movies]
@@ -11,19 +81,23 @@ const typeDefs = gql`
 
 type Movies {
  id: ID!
- name: String!
+ title: String!
  imageUrl: String!
  description: String! 
 }
 type Series{
  id: ID!
- name: String!
+ title: String!
  imageUrl: String!
  description: String!
+ saisons: Int 
 }
-
-
 `;
+const resolvers = {
+    Query: {
+        
+    }
+}
 
 const server = new ApolloServer({typeDefs});
 
